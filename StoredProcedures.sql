a1be080620c6b963f59cac9e332ef1cd11dba877
SrushtiGharat <71805523+SrushtiGharat@users.noreply.github.com>
2020-11-07 20:21:35 +0530

    Update StoredProcedures.sql

--- StoredProcedures.sql
+++ StoredProcedures.sql
@@ -30,3 +30,17 @@ begin
 update employee_payroll set basic_pay = @BasicPay where name = @EmployeeName
 
 END
+
+create or Alter procedure SpGetEmployeesByStartDateRange
+(
+@StartDate1 date,
+@StartDate2 date
+)
+AS
+BEGIN
+
+select id,name,basic_pay,start_date,gender,phone,address,department,
+deduction,taxable_pay,income_tax,net_pay from employee_payroll 
+where start_date between @StartDate1 and @StartDate2;
+
+END
