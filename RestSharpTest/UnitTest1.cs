2cd722a4e639f86b4e4f2d9e27bde9f73873a259
Srushti <Srushti@DESKTOP-QP0QMA4>
2020-11-16 20:00:36 +0530

    [Srushti]Add.Insert new employee in json server

--- RestSharpTest/UnitTest1.cs
+++ RestSharpTest/UnitTest1.cs
@@ -1,5 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using RestSharp;
 using System.Collections.Generic;
 
@@ -39,6 +40,24 @@ namespace RestSharpTest
             {
                 System.Console.WriteLine("id: " + item.id + "Name: " + item.name + "Salary: " + item.Salary);
             }
+        }
+
+        [TestMethod]
+        public void givenEmployee_OnPost_ShouldReturnAddedEmployee()
+        {
+            RestRequest request = new RestRequest("/employees", Method.POST);
+            JObject jObjectbody = new JObject();
+            jObjectbody.Add("name", "Clark");
+            jObjectbody.Add("Salary", "15000");
+            request.AddParameter("application/json", jObjectbody, ParameterType.RequestBody);
+
+            IRestResponse response = client.Execute(request);
+
+            Assert.AreEqual(response.StatusCode, System.Net.HttpStatusCode.Created);
+            Employee dataResponse = JsonConvert.DeserializeObject<Employee>(response.Content);
+            Assert.AreEqual("Clark", dataResponse.name);
+            Assert.AreEqual(15000, dataResponse.Salary);
+
         }
     }
 }
